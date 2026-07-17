5adbdc6200026d502bdc01b9b9bfeabbe2e2769f
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-07-17 12:37:00 +0530

    made the access toke time lesser to check

--- src/config/token.js
+++ src/config/token.js
@@ -4,7 +4,7 @@ const generateAccessToken = (userId, role) => {
     return jwt.sign(
         { id: userId, role: role },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '30m' }
+        { expiresIn: '1m' }
     )
 }
 
